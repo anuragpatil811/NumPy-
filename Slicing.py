505ef69ec0462f9b6be56e81f47a211ea851b026
Anurag Arun patil <anuragpatil811@gmail.com>
2025-11-27 22:35:36 +0530

    Add files via upload

---
+++ Slicing.py
@@ -0,0 +1,13 @@
+import numpy as np
+var = np.array([1, 2, 3, 4, 5, 6, 7])
+#Index numbers  0  1  2  3  4  5  6
+#print(var)
+#print()
+#print("2 to 5", var[1:5])
+#print("2 to End:", var[1:])
+#print("start to 5:", var[:5])
+#print("stop:", var[::2])
+#print("stop", var[1:6:2])
+
+var1 = np.array([[1, 2, 3, 4, 5], [9,8, 7, 6, 5], [11, 12, 13, 14, 15]])
+print("8 to 5", var1[2, 1:])
